7797394e7ac13f45ff11c53815d074976db4ab93
sunil sharma <129640411+sunil1289@users.noreply.github.com>
2025-07-25 16:35:11 +0545

    Update test-twilio.js

--- server/test-twilio.js
+++ server/test-twilio.js
@@ -1,7 +1,1 @@
-import twilio from 'twilio';
-const client = twilio("AC8ef66528297b3d960fc8b6d5c384fa48", "908bf4317aded3fdbf01825d700e921a");
 
-client.verify.services("VA2f1a78304904cf08ffea02be947613c8")
-  .verifications.create({ to: "cybersharma073@gmail.com", channel: "mail" })
-  .then(result => console.log(result))
-  .catch(err => console.error(err));
